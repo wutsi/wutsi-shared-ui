f9c363a9f4a51746644d7a986a6714b08785d352
wutsi <herve.tchepannou@wutsi.com>
2022-01-11 18:40:55 -0500

    remove apis

--- src/main/kotlin/com/wutsi/application/shared/WutsiBffApplication.kt
+++ src/main/kotlin/com/wutsi/application/shared/WutsiBffApplication.kt
@@ -1,9 +1,7 @@
 package com.wutsi.application.shared
 
-import com.wutsi.application.shared.config.AccountApiConfiguration
 import com.wutsi.application.shared.config.PhoneNumberConfiguration
 import com.wutsi.application.shared.config.ServiceConfiguration
-import com.wutsi.application.shared.config.TenantApiConfiguration
 import com.wutsi.application.shared.config.TogglesConfiguration
 import org.springframework.context.annotation.Import
 
@@ -11,9 +9,7 @@ import org.springframework.context.annotation.Import
 @Target(AnnotationTarget.CLASS)
 @Import(
     value = [
-        AccountApiConfiguration::class,
         ServiceConfiguration::class,
-        TenantApiConfiguration::class,
         TogglesConfiguration::class,
         PhoneNumberConfiguration::class
     ]
