61b97370957174a06cc9f96750ff810984388745
wutsi <herve.tchepannou@wutsi.com>
2022-03-04 10:43:06 -0500

    fix imageURL return

--- src/main/kotlin/com/wutsi/application/shared/service/QrService.kt
+++ src/main/kotlin/com/wutsi/application/shared/service/QrService.kt
@@ -6,7 +6,7 @@ import org.springframework.core.env.Profiles
 open class QrService(
     private val env: Environment
 ) {
-    open fun imageUrl(token: String): String? {
+    open fun imageUrl(token: String): String {
         val env = if (env.acceptsProfiles(Profiles.of("prod")))
             com.wutsi.platform.qr.Environment.PRODUCTION
         else
