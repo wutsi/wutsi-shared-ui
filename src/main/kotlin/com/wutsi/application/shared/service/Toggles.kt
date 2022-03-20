d4859caca590ae3c87e2ee7cdd113a1aad2e23ab
wutsi <herve.tchepannou@wutsi.com>
2022-03-20 10:58:18 -0400

    update toggles

--- src/main/kotlin/com/wutsi/application/shared/service/Toggles.kt
+++ src/main/kotlin/com/wutsi/application/shared/service/Toggles.kt
@@ -1,20 +1,20 @@
 package com.wutsi.application.shared.service
 
 open class Toggles {
-    var account: Boolean = true
-    var business: Boolean = true
-    var cart: Boolean = true
-    var contact: Boolean = true
-    var digitalProduct: Boolean = true
-    var feedback: Boolean = true
-    var logout: Boolean = true
-    var payment: Boolean = true
-    var scan: Boolean = true
-    var sendSmsCode: Boolean = true
-    var shippingInternational: Boolean = true
-    var store: Boolean = true
-    var switchEnvironment: Boolean = true
-    var verifySmsCode: Boolean = true
+    var account: Boolean = false
+    var business: Boolean = false
+    var cart: Boolean = false
+    var contact: Boolean = false
+    var digitalProduct: Boolean = false
+    var feedback: Boolean = false
+    var logout: Boolean = false
+    var payment: Boolean = false
+    var scan: Boolean = false
+    var sendSmsCode: Boolean = false
+    var shippingInternational: Boolean = false
+    var store: Boolean = false
+    var switchEnvironment: Boolean = false
+    var verifySmsCode: Boolean = false
 
     var testerUserIds: List<Long> = emptyList()
     var testPhoneNumbers: List<String> = emptyList()
