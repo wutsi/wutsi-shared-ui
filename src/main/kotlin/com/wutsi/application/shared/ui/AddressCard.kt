2783fe928c65477a012c10cfed4a20dd1a34645d
wutsi <herve.tchepannou@wutsi.com>
2022-05-18 11:09:42 -0400

    improve address layout

--- src/main/kotlin/com/wutsi/application/shared/ui/AddressCard.kt
+++ src/main/kotlin/com/wutsi/application/shared/ui/AddressCard.kt
@@ -1,5 +1,6 @@
 package com.wutsi.application.shared.ui
 
+import com.wutsi.application.shared.Theme
 import com.wutsi.application.shared.model.AddressModel
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Text
@@ -9,15 +10,19 @@ import com.wutsi.flutter.sdui.enums.MainAxisAlignment
 
 class AddressCard(
     private val model: AddressModel,
+    private val textSize: Double = Theme.TEXT_SIZE_DEFAULT,
 ) : CompositeWidgetAware() {
     override fun toWidgetAware(): WidgetAware = Column(
         mainAxisAlignment = MainAxisAlignment.start,
         crossAxisAlignment = CrossAxisAlignment.start,
         children = listOfNotNull(
-            Text(model.fullName.trim()),
-            model.street?.let { Text(it, maxLines = 3) },
-            Text(model.location),
-            model.zipCode?.let { Text(it) }
+            toText(model.fullName.trim(), bold = true),
+            model.street?.let { toText(it, maxLines = 3) },
+            toText(model.location),
+            model.zipCode?.let { toText(it) }
         )
     )
+
+    private fun toText(caption: String, maxLines: Int? = null, bold: Boolean? = null): WidgetAware =
+        Text(caption, size = textSize, maxLines = maxLines, bold = bold)
 }
--- src/main/kotlin/com/wutsi/application/shared/ui/ShippingCard.kt
+++ src/main/kotlin/com/wutsi/application/shared/ui/ShippingCard.kt
@@ -1,9 +1,9 @@
 package com.wutsi.application.shared.ui
 
+import com.wutsi.application.shared.Theme
 import com.wutsi.application.shared.model.ShippingModel
 import com.wutsi.application.shared.service.TranslationUtil.getText
 import com.wutsi.flutter.sdui.Column
-import com.wutsi.flutter.sdui.Container
 import com.wutsi.flutter.sdui.Text
 import com.wutsi.flutter.sdui.WidgetAware
 import com.wutsi.flutter.sdui.enums.CrossAxisAlignment
@@ -11,6 +11,9 @@ import com.wutsi.flutter.sdui.enums.MainAxisAlignment
 
 class ShippingCard(
     private val model: ShippingModel,
+    private val textSize: Double = Theme.TEXT_SIZE_DEFAULT,
+    private val showShoppingInstructions: Boolean = true,
+    private val showExpectedDeliveryDate: Boolean = true,
 ) : CompositeWidgetAware() {
     override fun toWidgetAware(): WidgetAware = Column(
         mainAxisAlignment = MainAxisAlignment.start,
@@ -18,22 +21,18 @@ class ShippingCard(
         children = listOfNotNull(
             Text(
                 model.type +
-                    if (model.expectedDelivered != null)
+                    if (showExpectedDeliveryDate && model.expectedDelivered != null)
                         " - " + getText(
                             key = "shared-ui.shipping-cart.expected-delivery-date",
-                            args = arrayOf(model.expectedDelivered)
+                            args = arrayOf(model.expectedDelivered),
                         )
                     else
-                        ""
+                        "",
+                size = textSize
             ),
 
-            if (model.message != null)
-                Container(padding = 10.0)
-            else
-                null,
-
-            if (model.message != null)
-                Text(model.message, maxLines = 5)
+            if (showShoppingInstructions && model.message != null)
+                Text(model.message, maxLines = 5, size = textSize)
             else
                 null,
         )
