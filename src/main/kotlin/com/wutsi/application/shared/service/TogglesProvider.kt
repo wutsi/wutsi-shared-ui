aeab979b55773f8b26f7d2cb22e7aa3116f5833e
wutsi <herve.tchepannou@wutsi.com>
2022-06-03 11:17:21 -0400

    enable payment

--- src/main/kotlin/com/wutsi/application/shared/service/TogglesProvider.kt
+++ src/main/kotlin/com/wutsi/application/shared/service/TogglesProvider.kt
@@ -62,6 +62,6 @@ open class TogglesProvider(
     open fun isSwitchEnvironmentEnabled(): Boolean =
         isToggleEnabled(ToggleName.SWITCH_ENVIRONMENT)
 
-    private fun isToggleEnabled(toggle: ToggleName): Boolean =
+    fun isToggleEnabled(toggle: ToggleName): Boolean =
         tenantProvider.get().toggles.find { it.name.equals(toggle.name, true) } != null
 }
